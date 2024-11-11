f35e53bbdec6ec07349a0473cef166066389cbdb
Eduardo Marra <eduardomarra@gmail.com>
2024-11-11 18:10:08 -0300

    Add files via upload

---
+++ src/test/java/com/emarra/EmarraShop/EmarraShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emarra.EmarraShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmarraShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
